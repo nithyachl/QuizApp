9446f1a3e68c28a8f3751934c1280586f630f57f
Nithya Chintalapati <nithya.ch333@gmail.com>
2020-03-10 19:24:52 -0400

    Lab06b

--- app/src/main/java/com/chintalapatinithya/firstapp/quizapp/MainActivity.java
+++ app/src/main/java/com/chintalapatinithya/firstapp/quizapp/MainActivity.java
@@ -17,30 +17,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        submitButton=findViewById(R.id.clickButton);
-        responseText=findViewById(R.id.responseEditText);
-        displayText=findViewById(R.id.textBox);
-        submitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                System.out.println("Hello mom");
-                //Log.i("testButton","Heyy!");
-                Log.i("testButton","Heyy! "+responseText.getText().toString());
-                displayText.setText(displayText.getText().toString()+" "+responseText.getText().toString());
-            }
-        });
-
-        responseText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if(!hasFocus){
-                    if(responseText.getText().toString().equals("TJ")){
-                        displayText.setText("TJ Rocks!");
-                        responseText.setText("");
-                        responseText.setHint("That's a good name.");
-                    }
-                }
-            }
-        });
+//
+//        submitButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                System.out.println("Hello mom");
+//                //Log.i("testButton","Heyy!");
+//                Log.i("testButton","Heyy! "+responseText.getText().toString());
+//                displayText.setText(displayText.getText().toString()+" "+responseText.getText().toString());
+//            }
+//        });
+//
+//        responseText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+//            @Override
+//            public void onFocusChange(View v, boolean hasFocus) {
+//                if(!hasFocus){
+//                    if(responseText.getText().toString().equals("TJ")){
+//                        displayText.setText("TJ Rocks!");
+//                        responseText.setText("");
+//                        responseText.setHint("That's a good name.");
+//                    }
+//                }
+//            }
+//        });
     }
 }
---
+++ app/src/main/java/com/chintalapatinithya/firstapp/quizapp/StartPage.java
@@ -0,0 +1,32 @@
+package com.chintalapatinithya.firstapp.quizapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class StartPage extends AppCompatActivity {
+
+    TextView startBox;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        startBox = findViewById(R.id.startBox);
+
+        startBox.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(StartPage.this, MainActivity.class));
+            }
+        });
+
+    }
+
+}
